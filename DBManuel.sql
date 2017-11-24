887d0e57e7ae98b7459a3106d5c4ab9ee98d472f
mjhita <mjhita@telefonica.net>
2017-11-24 21:52:09 +0100

    Añadido fichero creación base de datos

---
+++ DBManuel.sql
@@ -0,0 +1,19 @@
+-- 
+--
+
+DROP DATABASE IF EXISTS DBManuel;
+CREATE DATABASE DBManuel;
+GRANT ALL ON DBManuel.* TO 'manuel'@'localhost' IDENTIFIED BY 'hita';
+USE DBManuel;
+--
+DROP TABLE IF EXISTS Amigos;
+
+CREATE TABLE Amigos (
+    Amigo_no    INT        , 
+    Nombre      VARCHAR(40), 
+    Apellidos   VARCHAR(40), 
+    EnComun     VARCHAR(40), 
+    Telefono    VARCHAR(20), 
+    Email       VARCHAR(40)
+);
+ 
