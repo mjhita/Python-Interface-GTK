bd8ea10c39ee217745d50252ed063631d217fb5a
mjhita <mjhita@telefonica.net>
2017-12-12 21:03:10 +0100

    Mejoras en la lista, operaciones

--- amigos_manuel.py
+++ amigos_manuel.py
@@ -24,7 +24,8 @@ class AmigosManuelGUI:
                         "onBorrarAmigo": self.onBorrarAmigo,
                         "onAcercaDe": self.onAcercaDe,
                         "onBtnOperacionClicked": self.onBtnOperacionClicked,
-			"onCloseAboutDialog": self.onCloseAboutDialog}
+			"onCloseAboutDialog": self.onCloseAboutDialog,
+                        "onCambiarSeleccion": self.onCambiarSeleccion}
         # Conectamos las señales e iniciamos la aplicación
         self.builder.connect_signals(self.handlers)
 
@@ -53,11 +54,9 @@ class AmigosManuelGUI:
         column.set_resizable(True)   
         self.lista.append_column(column)
         
-        # self.window.connect("delete_event", Gtk.main_quit)
         self.window.show_all()
         self.modoOperacion = 0  # No operacio, 1 Crear Amigo, 2 Obtener, 3 Actual, 4 Borrar
         self.ponModoOperacion() 
-        #self.window.resize(600,300)
         self.Conexion = None
         self.Cursor = None
         self.conectarABaseDatos()
@@ -69,13 +68,9 @@ class AmigosManuelGUI:
             self.builder.get_object("menuitemActualizar").set_sensitive(False)
             self.builder.get_object("menuitemBorrar").set_sensitive(False)
         else:
-            self.cargaLista()
+            self.cargarLista()
             self.activarCamposEdicion(False)
             self.idSeleccionado = -1
-   
-    def onDeleteWindow(self, *args):
-        self.cerrarBaseDatos()
-        Gtk.main_quit(*args)
 
     def conectarABaseDatos(self):
         try:
@@ -93,23 +88,27 @@ class AmigosManuelGUI:
         except MySQLdb.Error, e:
             mensajeErrorSQL(e)  
 
-    def cargaLista(self):
+    def cargarLista(self):
+        """
+            Carga la lista, el TreeView, con el Id y nombre de cada amigo por fila
+        """ 
         self.modeloLista.clear()
         query= "SELECT Amigo_no,Nombre FROM Amigos WHERE 1;"
         try:
             self.Cursor.execute(query)
             registros = self.Cursor.fetchall()
-        except MySQLdb.Error, e:
-            mensajeErrorSQL(e)
-            return
-        try:
             for registro in registros:
-                print "Introduce  registro", str(registro[0]), registro[1]
                 self.modeloLista.append([str(registro[0]),registro[1]])
                 
+        except MySQLdb.Error, e:
+            mensajeErrorSQL(e)
         except:
             self.statusbar.push(self.idStatusBar, "ERROR no se ha podido cargar la lista de amigos")
-            
+ 
+    def onDeleteWindow(self, *args):
+        self.cerrarBaseDatos()
+        Gtk.main_quit(*args)
+       
     def onCrearAmigo(self, menuitem ):
         self.modoOperacion = 1
         self.ponModoOperacion()
@@ -119,7 +118,6 @@ class AmigosManuelGUI:
         self.ponModoOperacion()
     
     def onActualizarAmigo(self, menuitem):
-        print "Actualizar Amigo"
         self.modoOperacion = 3
         self.ponModoOperacion()
 
@@ -127,8 +125,12 @@ class AmigosManuelGUI:
         self.modoOperacion = 4
         self.ponModoOperacion()
 
+    def onCambiarSeleccion(self,reeSelection):
+        self.cargarAmigo()
+
     def idAmigoNuevo(self):
-        """ Devuelve un ID para la inserción de un registro nuevo
+        """
+             Devuelve un ID para la inserción de un registro nuevo
 
              Obtiene el numero de registros de la base de datos y le suma 1
         
@@ -140,7 +142,14 @@ class AmigosManuelGUI:
         while next((fila for fila in self.modeloLista if int(fila[0]) == n_reg),None) != None :
             n_reg += 1
         return n_reg
-    
+      
+    def activarCamposEdicion(self, activar):
+        self.edNombre.set_sensitive(activar)
+        self.edEnComun.set_sensitive(activar)
+        self.edProcedencia.set_sensitive(activar)
+        self.edTelefono.set_sensitive(activar)
+        self.edEmail.set_sensitive(activar)
+            
     def ponerDatosEnBlanco(self):
         self.edNombre.set_text("")
         self.edEnComun.set_text("")
@@ -148,36 +157,69 @@ class AmigosManuelGUI:
         self.edTelefono.set_text("")
         self.edEmail.set_text("")
 
-    def ponerDatosEdicion(self):
-        query= "SELECT * FROM Amigos WHERE Amigo_no={:s};".format(self.idSeleccionado)
-        try:
-            self.Cursor.execute(query)
-            registro = self.Cursor.fetchone()
-        except MySQLdb.Error, e:
-            mensajeErrorSQL(e)
-            self.statusbar.push(self.idStatusBar, "ERROR no se ha podido cargar amigo")
-            return
+    def cargarDatosRegistro(self, registro):
+        """
+            Vuelca los datos del registro "registro" en los campos de edición de la
+            ventana grafica
+        """
         self.edNombre.set_text(registro[1])
         self.edEnComun.set_text(registro[3])
         self.edProcedencia.set_text(registro[2])
         self.edTelefono.set_text(registro[4])
         self.edEmail.set_text(registro[5])
+        
+    def ponerDatosEdicion(self):
+        """
+           Vuelca los datos del registro que coincida (por su ID) con el
+           seleccionado en la lista
+        """  
+        if not self.idSeleccionado:
+            self.ponerDatosEnBlanco()
+            return
+        query= "SELECT * FROM Amigos WHERE Amigo_no={:s};".format(self.idSeleccionado)
+        try:
+            if self.Cursor.execute(query) > 0:
+                registro = self.Cursor.fetchone()
+                self.cargarDatosRegistro(registro)
+            else:
+                self.ponerDatosEnBlanco()    
+        except MySQLdb.Error, e:
+            mensajeErrorSQL(e)
+            self.statusbar.push(self.idStatusBar, "ERROR no se ha podido cargar amigo")
+            return  
 
     def obtenerIdSeleccionado(self):
+        """
+            Obtiene de la lista el ID del elemento seleccionado y lo almacena en
+            idSeleccionado
+        """
         selection = self.lista.get_selection()
         tree_model, tree_iter = selection.get_selected()
-        self.idSeleccionado = tree_model.get_value(tree_iter, 0)
+        if tree_iter:
+            self.idSeleccionado = tree_model.get_value(tree_iter, 0)
+        else:
+            self.idSeleccionado = None
         
-    def activarCamposEdicion(self, activar):
-        self.edNombre.set_sensitive(activar)
-        self.edEnComun.set_sensitive(activar)
-        self.edProcedencia.set_sensitive(activar)
-        self.edTelefono.set_sensitive(activar)
-        self.edEmail.set_sensitive(activar)
-        
+    def ponerIdSeleccionado(self, idSel):
+        """
+           Selecciona de la lista (TreeView) la fila con la columna ID =idSel
+
+        """
+        selection = self.lista.get_selection()
+        cont = 0
+        for fila in self.modeloLista:
+           if fila[0] == idSel:
+               break
+           cont += 1
+         
+        selection.select_path(cont)   
+
     def crearAmigo(self):
+        """
+            Crea un amigo nuevo y lo añade a la tabla y a la lista de amigos
+        """ 
         nombre = self.edNombre.get_text()
-        if nombre.isspace():
+        if nombre.isspace() or nombre == "":
             self.statusbar.push(self.idStatusBar, "ERROR Debe introducir al menos el nombre")
         else:
             idAmigo = self.idAmigoNuevo()
@@ -193,19 +235,46 @@ class AmigosManuelGUI:
                     self.statusbar.push(self.idStatusBar, "ERROR no se ha podido crear amigo")
                 else:
                     self.Conexion.commit()
-                    self.cargaLista()
+                    self.cargarLista()
                     self.ponerDatosEnBlanco()
-                    self.activarCamposEdicion(False)
-                    self.statusbar.push(self.idStatusBar, "Se ha creado un amigo nuevo")
             except MySQLdb.Error, e:
                 mensajeErrorSQL(e)
-                self.statusbar.push(self.idStatusBar, "ERROR no se ha podido crear amigo")     
+                self.statusbar.push(self.idStatusBar, "ERROR no se ha podido crear amigo")
+        self.activarCamposEdicion(False)       
 
-    def obtenerAmigo(self):
+    def cargarAmigo(self):
+        """
+            Obtiene el ID del amigo de la lista, lo busca en la tabla y lo carga en los
+            campos de edición de datos
+        """
         self.obtenerIdSeleccionado()
         self.ponerDatosEdicion()
-        
+
+    def obtenerAmigo(self):
+        """
+           Solicita un nombre de amigo y lo busca en la tabla. Si lo encuentra vuelca
+           sus datos en los campos de edición de la ventana
+        """
+        nombre = self.edNombre.get_text()
+        query= 'SELECT * FROM Amigos WHERE Nombre="{:s}";'.format(nombre)
+        try:
+            nSel = self.Cursor.execute(query)
+            if nSel > 0:
+                registro = self.Cursor.fetchone()
+                self.cargarDatosRegistro(registro)
+                self.ponerIdSeleccionado(str(registro[0]))
+            else:
+                self.ponerDatosEnBlanco()
+                self.edNombre.set_text("No se ha encontrado")
+            self.edNombre.set_sensitive(False)    
+        except MySQLdb.Error, e:
+            mensajeErrorSQL(e)
+            self.statusbar.push(self.idStatusBar, "ERROR no se ha podido cargar amigo")
+            
     def actualizarAmigo(self):
+        """
+           Permite modificar los datos del amigo que esté seleccionado
+        """
         nombre = self.edNombre.get_text()
         if nombre.isspace():
             self.statusbar.push(self.idStatusBar, "ERROR el nombre no puede estar vacio")
@@ -222,15 +291,17 @@ class AmigosManuelGUI:
             try:
                 self.Cursor.execute(query)
                 self.Conexion.commit()
-                self.cargaLista()
-                self.ponerDatosEnBlanco()
-                self.activarCamposEdicion(False)
-                self.statusbar.push(self.idStatusBar, "Se han modificado los datos del amigo")
+                self.cargarLista()
+                self.ponerDatosEdicion()
             except MySQLdb.Error, e:
                 mensajeErrorSQL(e)
                 self.statusbar.push(self.idStatusBar, "ERROR no se han podido modificar los datos del amigo")
+        self.ponerDatosEnBlanco()    
 
     def borrarAmigo(self):
+        """
+           Borra el amigo que esté seleccionado
+        """
         self.obtenerIdSeleccionado()
         query = 'DELETE FROM Amigos WHERE Amigo_no="{:s}"'.format(self.idSeleccionado)
         try:
@@ -238,8 +309,8 @@ class AmigosManuelGUI:
                 self.statusbar.push(self.idStatusBar, "ERROR no se ha podido borrar el amigo")
             else:
                 self.Conexion.commit()
-                self.cargaLista()
-                self.statusbar.push(self.idStatusBar, "Amigo eliminado")
+                self.cargarLista()
+            self.ponerDatosEnBlanco()    
         except MySQLdb.Error, e:
             mensajeErrorSQL(e)
             self.statusbar.push(self.idStatusBar, "ERROR no se ha podido borrar el amigo")
@@ -247,13 +318,18 @@ class AmigosManuelGUI:
     def onBtnOperacionClicked(self, button):
         if self.modoOperacion == 1:
             self.crearAmigo()
-            time.sleep(2)  
         elif self.modoOperacion == 2:
             self.obtenerAmigo()
         elif self.modoOperacion == 3:
-            self.actualizarAmigo()
+            self.obtenerIdSeleccionado()
+            if self.idSeleccionado:
+                self.actualizarAmigo()
+            else:
+                self.ponerDatosEnBlanco()
+            self.activarCamposEdicion(False)    
         elif self.modoOperacion == 4:
-            self.borrarAmigo()
+            if self.idSeleccionado:
+                self.borrarAmigo()
   
         self.modoOperacion = 0
         self.ponModoOperacion()
@@ -262,7 +338,6 @@ class AmigosManuelGUI:
         self.about.show()
    
     def onCloseAboutDialog(self,window,data=None):
-        print "Salir Acerca de"
         self.about.hide()
 
     def ponModoOperacion(self):
@@ -270,27 +345,29 @@ class AmigosManuelGUI:
             self.btOperacion.hide()
             self.statusbar.push(self.idStatusBar, "Elija una opción del menú Operaciones.")
         elif self.modoOperacion == 1:
+            self.btOperacion.set_label("Crear nuevo amigo")
             self.activarCamposEdicion(True)
             self.ponerDatosEnBlanco()
             self.btOperacion.show()
-            self.btOperacion.set_label("Crear nuevo amigo")
             self.statusbar.push(self.idStatusBar,
                                 "Introduzca los datos de un nuevo amigo y pulse el botón 'Crear nuevo amigo'")
         elif self.modoOperacion == 2:
-            self.btOperacion.show()
             self.btOperacion.set_label("Obtener amigo")
+            self.btOperacion.show()
+            self.ponerDatosEnBlanco()
+            self.edNombre.set_sensitive(True)
             self.statusbar.push(self.idStatusBar,
-                                "Seleccione amigo de la lista y pulse el botón 'Obtener amigo'")
+                                "Introduzca nombre de amigo y pulse el botón 'Obtener amigo'")
         elif self.modoOperacion == 3:
+            self.btOperacion.set_label("Actualizar datos")
             self.btOperacion.show()
             self.activarCamposEdicion(True)
-            self.obtenerAmigo()
-            self.btOperacion.set_label("Actualizar datos")
+            self.cargarAmigo()
             self.statusbar.push(self.idStatusBar,
                                 "Modifique los datos del amigo seleccionado y pulse 'Actualizar datos'")
         elif self.modoOperacion == 4:
-            self.btOperacion.show()
             self.btOperacion.set_label("Borrar amigo")
+            self.btOperacion.show()
             self.statusbar.push(self.idStatusBar,
                                 "Pulse 'Borrar amigo' y se eliminarán los datos del amigo seleccionado")
             
